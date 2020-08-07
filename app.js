c28c68a90c0143776e3844edbd3f7b569d223da8
Cedric Mensah <cedric@mensah.de>
2020-08-07 11:20:11 +0200

    Added body parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -18,6 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -28,6 +30,14 @@ app.use('/spotify_api', dataRouter)
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
+// body parser
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
