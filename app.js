c0c36889823dc0d368b7cfb841e1a97d88c1b293
Cedric Mensah <cedric@mensah.de>
2023-11-02 14:18:12 +0100

    commented out appInsights

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var appInsights = require('applicationinsights');
+// var appInsights = require('applicationinsights');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
